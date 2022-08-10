b440640e581acd12573c3a694546d7365b81cd78
SNOCK-MAC <snock-mac@SNOCK-MACs-MacBook-Pro.local>
2022-08-10 16:50:58 +0700

    complete variation table

--- pages/itemslibrary.js
+++ pages/itemslibrary.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { Button, Checkbox, Input, Dialog, DialogHeader, DialogBody, DialogFooter } from "@material-tailwind/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImage, faX } from '@fortawesome/free-solid-svg-icons'
+import { isConstructorDeclaration } from 'typescript';
 
 const itemslibrary = () => {
     const mock_data = [
@@ -26,29 +27,72 @@ const itemslibrary = () => {
         {id: 6,name: 'Price'},
         {id: 5,name: 'Stock'},
     ])
-    const [createModalToggle, setCreateModalToggle] = useState(true)
+    const [createModalToggle, setCreateModalToggle] = useState(false)
     const [variationsItems, setVariationsItems] = useState([])
+    const [variationsCheckedAll, setVariationsCheckedAll] = useState(false)
 
     useEffect(() => {
-        generateingVariations()
+        // generateingVariations()
     }, [])
 
     const generateingVariations = () => {
         const cartesian = (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())))
+        let output = []
         let mainArray = []
         mock_options.map(item => {
             mainArray.push(item.option)
         })
 
-        setVariationsItems(cartesian(...mainArray))
-        // console.log('generateingVariations: ', cartesian(...mainArray))
+        //Prepair Information
+        cartesian(...mainArray).map((item, idx)=> {
+            output.push({id: idx,variant: item, sku: '', price: '', location: '',variationsCheckedItem: false,openPriceCol: false})
+        })
+        setVariationsItems(output)
+    }
+
+    const handleChange_Variations = (element,index) => {
+        const {type,name,value,checked} = element.target
+        
+        //set item data
+        let arr = [...variationsItems]
+        let objIndex = arr.findIndex(x => x.id === index)
+        
+        if(name === 'price') {
+            //check mutiple column
+            if(arr_itemsChecked.length > 1) {
+                arr[objIndex].variationsCheckedItem === true ? arr.map(item => {arr_itemsChecked.map(subItem => {if(item.id === subItem.id) item.price = value})}) : arr[objIndex][name] = value
+            } else {
+                arr[objIndex][name] = value
+            }
+        } else {
+            arr[objIndex][name] = type === 'checkbox' ? checked : value
+        }
+        
+        //set checked all
+        let arr_itemsChecked = variationsItems.filter(x => x.variationsCheckedItem === true)
+        setVariationsCheckedAll(arr_itemsChecked.length > 0 ? true : false)
+        setVariationsItems(arr)
+    }
+
+    const handleRemove_Variations = (index) => {
+        let arr = [...variationsItems]
+        arr = arr.filter(x => x.id !== index)
+
+        let arr_itemsChecked = arr.filter(x => x.variationsCheckedItem === true)
+        setVariationsCheckedAll(arr_itemsChecked.length > 0 ? true : false)
+        setVariationsItems(arr)
     }
 
     const handleOpenCreateModal = () => {
         setCreateModalToggle(!createModalToggle)
+    }
 
-        // For Test
-        generateingVariations()
+    const handleCheckAll_Variations = (element) => {
+        const {checked} = element.target
+        let arr = [...variationsItems]
+        arr.map(item => {item.variationsCheckedItem = checked})
+        setVariationsItems(arr)
+        setVariationsCheckedAll(checked)
     }
 
     return <>
@@ -58,6 +102,7 @@ const itemslibrary = () => {
                     <Input className="w-72" size="lg" label="Search" />
                 </div>
                 <div className="grow-0">
+                    <Button onClick={()=>generateingVariations()}>Mock Data</Button>
                     <Button onClick={()=>handleOpenCreateModal()}>Create an Item</Button>
                 </div>
             </div>
@@ -127,14 +172,24 @@ const itemslibrary = () => {
                             variationsItems.length > 0 ?
                                 <div className='flex flex-row text-gray-800 font-normal items-center border-b-2 py-1'>
                                     <div className=''>
-                                        <Checkbox color="blue" defaultChecked />
+                                        <Checkbox onChange={e=>{handleCheckAll_Variations(e)}} checked={variationsCheckedAll}/>
                                     </div>
                                     <div className='pt-1 pr-5'>
-                                        {variationsItems.length} Variations
+                                        {
+                                            variationsCheckedAll ?
+                                                variationsItems.filter(x => x.variationsCheckedItem === true).length + " of " + variationsItems.length
+                                            :
+                                                variationsItems.length + " Variations"
+                                        }
                                     </div>
-                                    <div className='pt-1 text-blue-600'>
-                                        Edit price
-                                    </div>
+                                    {
+                                        variationsCheckedAll ?
+                                            <div className='pt-1 text-blue-600'>
+                                                Edit price
+                                            </div>
+                                        :
+                                            <></>
+                                    }
                                 </div>
                             :
                                 <></>
@@ -158,12 +213,12 @@ const itemslibrary = () => {
                                     {
                                         variationsItems.map((item, idx) => {
                                             return (
-                                                <tr key={idx} className='divide-y-2 cursor-pointer'>
+                                                <tr key={item.id} className='divide-y-2 cursor-pointer'>
                                                     <td className='w-0'/>
-                                                    <td className='py-1 pl-2 border-r-2' onClick={()=>console.log('Click variations:', idx)}>
+                                                    <td className='py-1 pl-2 border-r-2' onClick={()=>console.log('Click variations:', item.id)}>
                                                         <div className='flex flex-row'>
                                                             <div>
-                                                                <Checkbox color="blue" defaultChecked />
+                                                                <Checkbox name="variationsCheckedItem" checked={item.variationsCheckedItem} onChange={e=>handleChange_Variations(e,item.id)}/>
                                                             </div>
                                                             <div className='flex flex-row items-center pl-2 text-blue-800'>
                                                                 <div className='flex bg-gray-100 w-10 h-10 rounded items-center justify-center mr-3 hover:bg-blue-300'>
@@ -171,24 +226,24 @@ const itemslibrary = () => {
                                                                 </div>
                                                             </div>
                                                             <div className='flex items-center'>
-                                                                {item.map((subItem, subId) => {
+                                                                {item.variant.map((subItem, subId) => {
                                                                     return (subItem.length-1 !== subId ? "," : "") + subItem
                                                                 })}
                                                             </div>
                                                         </div>
                                                     </td> 
                                                     <td className='py-1 px-2 border-r-2'>
-                                                        <input className='w-full focus:outline-none' placeholder="SKU"/>
+                                                        <input className='w-full focus:outline-none' placeholder="SKU" name='sku' value={item.sku} onChange={e=>handleChange_Variations(e,item.id)}/>
                                                     </td>
                                                     <td className='py-1 px-2 border-r-2'>
-                                                        <input className='w-full focus:outline-none'style={{direction: 'rtl'}} dir="auto" placeholder="$0.00"/>
+                                                        <input className='w-full text-right focus:outline-none' type="number" placeholder="$0.00" name='price' value={item.price} onChange={e=>handleChange_Variations(e,item.id)} disabled={item.openPriceCol}/>
                                                     </td>
-                                                    <td className='py-1 px-2 text-right' onClick={()=>console.log('Click manage:', idx)}>
+                                                    <td className='py-1 px-2 text-right' onClick={()=>console.log('Click manage:', item.id)}>
                                                         <div className='text-blue-800'>
                                                             Manage
                                                         </div>
                                                     </td>
-                                                    <td className='px-2 text-right' onClick={()=>console.log('Click delete:', idx)}>
+                                                    <td className='px-2 text-right' onClick={()=>{handleRemove_Variations(item.id)}}>
                                                         <Button className='' variant="text">
                                                             <FontAwesomeIcon className="text-base text-gray-300" icon={faX} />
                                                         </Button>
